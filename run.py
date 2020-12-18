8f1af3bee281a713c09ce6396dd3e6846767214a
Hiroshi Lin <hlee993@gmail.com>
2020-12-18 22:00:07 +0900

    view yesterday exchange rate & schedule time change

--- run.py
+++ run.py
@@ -18,10 +18,10 @@ def make_sentence(country="KRW"):
     today_rate = exchange_rate.get_today_exchange(country)
     yester_rate = exchange_rate.get_yesterday_exchange(country)
     if(country=="KRW"):
-        sentence = "오늘의 환율은 {:.2f}이고, 전일대비 {:.2f}달러, 등락률은 {:.2f}%입니다.".format(today_rate, 
+        sentence = "어제의 환율은{:.2f} 오늘의 환율은 {:.2f}이고, 전일대비 {:.2f}달러, 등락률은 {:.2f}%입니다.".format(yester_rate, today_rate, 
         exchange_rate.compare_yesterday(today_rate, yester_rate), exchange_rate.get_Fluctuation_rate(today_rate, yester_rate))
     else:
-        sentence = "今日の為替レートは{:.2f}であり、前日比{:.2f}ドル、騰落率は{:.2f}％です。".format(today_rate, 
+        sentence = "昨日の為替レートは{:.2f} 今日の為替レートは{:.2f}であり、前日比{:.2f}ドル、騰落率は{:.2f}％です。".format(yester_rate, today_rate, 
         exchange_rate.compare_yesterday(today_rate, yester_rate), exchange_rate.get_Fluctuation_rate(today_rate, yester_rate))
     return sentence
 
@@ -38,7 +38,7 @@ def start_job():
     except tweepy.TweepError as e:
         print(e.reason)
 
-schedule.every().day.at("15:00").do(start_job)
+schedule.every().day.at("18:00").do(start_job)
 while True:
     schedule.run_pending()
     time.sleep(60)
